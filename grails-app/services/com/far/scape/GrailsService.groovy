ace0b4908c513c485bcc7347d1fb6740ccac8d8d
Christian Oestreich <acetrike@yahoo.com>
2011-04-14 13:15:37 -0500

    Formatting code

--- grails-app/services/com/far/scape/GrailsService.groovy
+++ grails-app/services/com/far/scape/GrailsService.groovy
@@ -1,12 +1,12 @@
 package com.far.scape
 
-//import com.far.scape.scala.Cast
+import com.far.scape.scala.Cast
 
 class GrailsService {
 
     static transactional = true
 
-    def saveObject(JavaCast cast) {
+    def saveObject(Cast cast) {
       println "Grails Service saving object ${cast.class.name}"
       cast.save()
       true
